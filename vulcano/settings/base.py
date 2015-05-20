8345bb11a63edbd20100f20fdf64de5ee289f0b6
javier <salomeka4@gmail.com>
2015-05-20 13:49:29 -0500

    Separando settings

---
+++ vulcano/settings/base.py
@@ -0,0 +1,39 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_KEY = '#2!^dr(x&6)a(epea3sw(mztwl0^dxh(w!c3dmz8&gypm&l2*f'
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'vulcano.urls'
+
+WSGI_APPLICATION = 'vulcano.wsgi.application'
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
---
+++ vulcano/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+STATIC_URL = '/static/'
